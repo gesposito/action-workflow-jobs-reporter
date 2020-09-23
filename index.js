5c75965c2a07f9104f0fa404b88a5d4e11de53ce
Gianluca Esposito <g@esposi.to>
2020-09-23 13:12:42 +0200

    Add action

--- index.js
+++ index.js
@@ -1,21 +1,86 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
+const path = require("path");
+const fs = require("fs").promises;
+
+const { getJunitXml } = require("junit-xml");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const githubToken = core.getInput("github_token");
+    const reportPath = core.getInput("report_path");
+    const reportFilename = core.getInput("report_filename");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const octokit = github.getOctokit(githubToken);
+    const { repo, workflow, runId } = github.context;
 
-    core.setOutput('time', new Date().toTimeString());
+    const {
+      data: { jobs },
+    } = await octokit.actions.listJobsForWorkflowRun({
+      // owner
+      // repo
+      ...repo,
+      run_id: runId,
+    });
+    core.info(`${jobs.length} jobs found`);
+
+    const report = getReportFrom(workflow, jobs);
+    const XML = getJunitXml(report);
+
+    await fs.mkdir(reportPath);
+    const fullPath = path.join(reportPath, reportFilename);
+    core.info(`Writing XML report to ${fullPath}`);
+    await fs.writeFile(fullPath, XML);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
+function getReportFrom(workflow, jobs) {
+  const report = {
+    name: workflow,
+    suites: jobs.map((job) => {
+      const suite = {
+        name: job.name,
+        testCases: job.steps.map(getTestCase(workflow, job)),
+      };
+      return suite;
+    }),
+  };
+
+  return report;
+}
+
+function getTestCase(workflow, job) {
+  return (step) => {
+    let testCase = {
+      name: step.name,
+    };
+    switch (step.conclusion) {
+      case "failure":
+        testCase.failures = [
+          { message: `${workflow} - ${job.name} - ${step.name} ` },
+        ];
+        break;
+      case "cancelled":
+        break;
+      case "skipped":
+        testCase.skipped = true;
+        break;
+      case "success":
+        break;
+      default:
+        break;
+    }
+    return testCase;
+  };
+}
+
 run();
+
+module.exports = {
+  getReportFrom,
+  getJunitXml,
+};
--- index.test.js
+++ index.test.js
@@ -1,23 +1,82 @@
-const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+const { getReportFrom, getJunitXml } = require("./index");
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
+const data = {
+  total_count: 1,
+  jobs: [
+    {
+      id: 1133363344,
+      run_id: 260956307,
+      run_url:
+        "https://api.github.com/repos/gesposito/action-workflow-retry/actions/runs/260956307",
+      node_id: "MDg6Q2hlY2tSdW4xMTMzMzYzMzQ0",
+      head_sha: "dbc7f6f9990b89d3eda457d1174d8b183d75b8a2",
+      url:
+        "https://api.github.com/repos/gesposito/action-workflow-retry/actions/jobs/1133363344",
+      html_url:
+        "https://github.com/gesposito/action-workflow-retry/runs/1133363344",
+      status: "completed",
+      conclusion: "failure",
+      started_at: "2020-09-18T10:31:52Z",
+      completed_at: "2020-09-18T10:31:57Z",
+      name: "error",
+      steps: [
+        {
+          name: "Set up job",
+          status: "completed",
+          conclusion: "success",
+          number: 1,
+          started_at: "2020-09-18T12:31:52.000+02:00",
+          completed_at: "2020-09-18T12:31:54.000+02:00",
+        },
+        {
+          name: "Run actions/checkout@v2",
+          status: "completed",
+          conclusion: "success",
+          number: 2,
+          started_at: "2020-09-18T12:31:54.000+02:00",
+          completed_at: "2020-09-18T12:31:56.000+02:00",
+        },
+        {
+          name: "Run npm run error",
+          status: "completed",
+          conclusion: "failure",
+          number: 3,
+          started_at: "2020-09-18T12:31:56.000+02:00",
+          completed_at: "2020-09-18T12:31:57.000+02:00",
+        },
+        {
+          name: "Post Run actions/checkout@v2",
+          status: "completed",
+          conclusion: "success",
+          number: 6,
+          started_at: "2020-09-18T12:31:57.000+02:00",
+          completed_at: "2020-09-18T12:31:57.000+02:00",
+        },
+        {
+          name: "Complete job",
+          status: "completed",
+          conclusion: "success",
+          number: 7,
+          started_at: "2020-09-18T12:31:57.000+02:00",
+          completed_at: "2020-09-18T12:31:57.000+02:00",
+        },
+      ],
+      check_run_url:
+        "https://api.github.com/repos/gesposito/action-workflow-retry/check-runs/1133363344",
+    },
+  ],
+};
+
+describe("Report", () => {
+  let report;
+
+  test("Returns a JSON report from Workflow jobs", async () => {
+    report = getReportFrom("workflow name", data.jobs);
+    expect(report).toMatchSnapshot();
+  });
+
+  test("Returns a JUnit XML report from Workflow jobs", async () => {
+    const xml = getJunitXml(report);
+    expect(xml).toMatchSnapshot();
+  });
 });
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
-  const ip = path.join(__dirname, 'index.js');
-  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
-})
